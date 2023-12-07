4d9f7a7ca1c004115ae70f718b4958b3fde81da4
Michael Villalba Sotelo <michael.villalba@ab-inbev.com>
2023-12-06 18:10:00 -0600

    fix: add settings.json and icon component

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.css'
 })
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { IconComponent } from '../components/ui/icon/icon.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, IconComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
