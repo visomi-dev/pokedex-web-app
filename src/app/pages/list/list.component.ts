53bae0521ae8c83fd72cffcf7e704bb91e55a4f6
Michael Villalba Sotelo <michael.villalba@ab-inbev.com>
2023-12-13 18:01:58 -0600

    fix: update routes and components

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-import { IconComponent } from '../components/ui/icon/icon.component';
+import { IconComponent } from '../../components/ui/icon/icon.component';
 
 @Component({
   selector: 'app-list',
